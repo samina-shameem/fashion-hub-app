e104627e20acb0c044d2d0d494d23a70eadd45d8
samina-shameem <samina.shameem.clt@gmail.com>
2024-04-14 19:38:45 +0200

    Updated the product page

--- front_end/src/components/Product.jsx
+++ front_end/src/components/Product.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
-import ListGroup from "react-bootstrap/ListGroup"; // Added import statement for ListGroup
-import { Link } from "react-router-dom";
+import ListGroup from "react-bootstrap/ListGroup";
 
 function Product({ item }) {
-  const sizesArray = item.sizes.split(",");
+  const [selectedSizes, setSelectedSizes] = useState([]);
+
+  const toggleSize = (size) => {
+    setSelectedSizes((prevSelectedSizes) => {
+      if (prevSelectedSizes.includes(size)) {
+        return prevSelectedSizes.filter((selectedSize) => selectedSize !== size);
+      } else {
+        return [...prevSelectedSizes, size];
+      }
+    });
+  };
 
   return (
     <Container className="mb-4">
@@ -18,20 +27,23 @@ function Product({ item }) {
         </Card.Body>
         <ListGroup className="list-group-flush">
           <ListGroup.Item>
-            {sizesArray.map((size, index) => (
-              <Button key={index} variant="secondary" className="me-1 mb-1">
-                {size.trim()}
+            {item.sizes.split(",").map((size) => (
+              <Button
+                key={size}
+                variant={selectedSizes.includes(size) ? "primary" : "secondary"}
+                onClick={() => toggleSize(size)}
+                style={{ marginRight: "5px" }}
+              >
+                {size}
               </Button>
             ))}
           </ListGroup.Item>
           <ListGroup.Item>SEK: {item.price}</ListGroup.Item>
           <ListGroup.Item>
-            <Link to={`/product/${item.id}`} className="btn btn-primary">
-              More Details
-            </Link>
+            <Button variant="primary">More Details</Button>
           </ListGroup.Item>
           <ListGroup.Item>
-            <Button variant="primary"> add to cart </Button>
+            <Button variant="primary">Add to Cart</Button>
           </ListGroup.Item>
         </ListGroup>
       </Card>
