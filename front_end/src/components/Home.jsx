f40ccc0fdca16a642e7f6fc716285e867c6d3a52
samina-shameem <samina.shameem.clt@gmail.com>
2024-04-14 17:36:36 +0200

    Updated Home page

--- front_end/src/components/Home.jsx
+++ front_end/src/components/Home.jsx
@@ -1,26 +1,40 @@
-import React from "react";
-import Product from "./Product";
-import SearchForm from "./SearchForm";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import React, { useState } from "react";
+import { Button, Container, Row, Col, FormControl } from "react-bootstrap";
+import ProductList from "./ProductList";
 
 function Home() {
+  const [searchWord, setSearchWord] = useState("");
+  const [keyword, setKeyword] = useState("");
+
+  const handleSearchWord = (event) => {
+    const newSearchWord = event.target.value;
+    setSearchWord(newSearchWord);
+  };
+
+  const handleSearch = () => {
+    setKeyword(searchWord);
+  };
+
   return (
-    <div>
-      <SearchForm />
-      <Container>
-      <Row >        
-        <Col><Product /></Col>
-        <Col><Product /></Col>
-        <Col><Product /></Col>
-        <Col><Product /></Col>
-        <Col><Product /></Col>
-        <Col><Product /></Col>
-        <Col><Product /></Col>      
+    <Container>
+      <Row className="justify-content-md-center mt-3">
+        <Col md="6">
+          <FormControl
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+            value={searchWord}
+            onChange={handleSearchWord}
+          />
+        </Col>
+        <Col md="auto">
+          <Button variant="outline-success" onClick={handleSearch}>Search</Button>
+        </Col>
+      </Row>
+      <Row className="justify-content-md-center mt-3">
+        <ProductList searchWord={keyword} />
       </Row>
     </Container>
-    </div>
   );
 }
 
---
+++ front_end/src/components/ProductList.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function ProductList(props) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+ProductList.propTypes = {
+
+}
+
+export default ProductList
+
