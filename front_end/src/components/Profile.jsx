0ff1612680ede230f8a343cbed0ef76874bcbe46
samina-shameem <samina.shameem.clt@gmail.com>
2024-04-14 16:34:47 +0200

    Updated the profile bootstrap

--- front_end/src/components/Profile.jsx
+++ front_end/src/components/Profile.jsx
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import Alert from "react-bootstrap/Alert";
 import { UserLoginContext } from "../context/UserLoginProvider";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-function Profile({ msg }) { 
-  const { userLoggedIn, userName, setUserLoggedIn, setUserName } =
-    useContext(UserLoginContext);
+function Profile({ msg }) {
+  const { userLoggedIn, userName, setUserLoggedIn, setUserName } = useContext(UserLoginContext);
 
   const handleLogout = (event) => {
     setUserLoggedIn(false);
@@ -14,32 +16,44 @@ function Profile({ msg }) {
   };
 
   return (
-    <>
-      {userLoggedIn ? (
-        <>
-          <Alert variant="success">
-            <Alert.Heading>Profile:</Alert.Heading>
-            <p>{msg}</p> 
-            <hr />
-            <p>User name: {userName}</p>
-          </Alert>
-          <p>
-            <Button variant="primary" onClick={handleLogout}>
-              Log out
-            </Button>
-          </p>
-        </>
-      ) : (
-        <>
-          <Button as={Link} to="/login" variant="primary">
-            Log in
-          </Button>
-          <Button as={Link} to="/signup" variant="primary">
-            Sign up
-          </Button>
-        </>
-      )}
-    </>
+    <Container>
+      <Row className="justify-content-center">
+        <Col md={6} className="text-center">
+          {userLoggedIn ? (
+            <>
+              <Alert variant="success">
+                <Alert.Heading>Profile:</Alert.Heading>
+                <p>{msg}</p>
+                <hr />
+                <p>User name: {userName}</p>
+              </Alert>
+              <Button variant="primary" onClick={handleLogout}>
+                Log out
+              </Button>
+            </>
+          ) : (
+            <>
+            <Row>
+              <Alert variant="success">
+                <Alert.Heading>Log out success </Alert.Heading>
+              </Alert>
+              <Col md={3}>
+              <Button as={Link} to="/login" variant="primary">
+                Log in
+              </Button>
+              </Col >  
+              <Col md={3}>
+              <Button as={Link} to="/signup" variant="primary">
+                Sign up
+              </Button>
+              </Col>
+              </Row>
+            </>
+          )}
+        </Col>
+        
+      </Row>
+    </Container>
   );
 }
 
